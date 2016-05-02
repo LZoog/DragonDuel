23d836c717381e0465e4ef601620bf4b5765a18e
Lauren Zugai <lauren@zugai.com>
2016-05-02 18:21:23 -0500

    Quiz on signup works

--- config/passport.js
+++ config/passport.js
@@ -33,14 +33,51 @@ module.exports = function(passport) {
           if (user) {
             return done(null, false, req.flash('errorMessage', 'This username has been taken!'));
           } else {
+            var light = 0, dark = 0;
+            var team, power, color;
+
+            var q1 = req.body.q1;
+            if (q1 == 'sun') { light++;
+            } else { dark++; }
+            var q2 = req.body.q2;
+            if (q2 == 'fairy' || q2 == 'unicorn') { light++;
+            } else { dark++; }
+            var q3 = req.body.q3;
+            if (q3 == 'opal' || q3 == 'diamond' || q3 == 'pearl') { light ++;
+            } else { dark++; }
+            var q4 = req.body.q4;
+            if (q4 == 'fame' || q4 == 'achievement') { light++;
+            } else { dark++; }
+            var q5 = req.body.q5;
+            if (q5 == 'no') { light++;
+            } else { dark++; }
+
+            var rand = Math.floor(Math.random()*3);
+
+            if (light > dark) {
+              team = 'Light';
+              var powers = ['Lightning', 'Fire', 'Mind Reading'];
+              var colors = ['Yellow', 'Orange', 'Green'];
+              power = powers[rand];
+              color = colors[rand];
+            } else {
+              team = 'Dark';
+              var powers = ['Darkness', 'Ice', 'Psychic'];
+              var colors = ['Red', 'Blue', 'Purple']
+              power = powers[rand];
+              color = colors[rand];
+            }
+
             var newUser = new User();
             newUser.local.email = email;
             newUser.local.password = User.hash(password);
             newUser.local.username = req.body.username;
             newUser.local.gender = req.body.gender;
+            newUser.local.color = color;
+            newUser.local.power = power;
+            newUser.local.team = team;
 
             newUser.save(function(err, user) {
-              console.log(err);
               if (err) return done(err);
               return done(null, user);
             });
