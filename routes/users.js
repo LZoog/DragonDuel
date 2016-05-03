f83c5c07c8f51866b176944dade91eeb69b383dd
Lauren Zugai <lauren@zugai.com>
2016-05-03 10:20:35 -0500

    Changed SCSS, now accounts for different levels on user show page

--- routes/users.js
+++ routes/users.js
@@ -6,7 +6,7 @@ router.get('/:username', function(req, res, next) {
   var username = req.params.username;
   User.findOne({ 'local.username': username}, function(err, user) {
     if (err) console.log(err);
-    res.render('user', {username: user.local.username, gender: user.local.gender, team: user.local.team, color: user.local.color});
+    res.render('user', {username: user.local.username, gender: user.local.gender, level: user.local.level, team: user.local.team, color: user.local.color});
   })
 });
 
