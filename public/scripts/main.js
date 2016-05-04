a2a9849e590bfff4cb1dc0d6c7597a7b761dd0c5
Lauren Zugai <lauren@zugai.com>
2016-05-03 19:41:36 -0500

    Working on getting Socket.IO working...

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -1,8 +1,15 @@
-var socket = io();
-
-socket.on('connect', function() {
-  console.log('Client connected!');
-});
-
 $(function() {
+  $('#enter').click(function(){
+    $('#battlefront').toggleClass('hide');
+    $('#battlefield').toggleClass('hide');
+    var socket = io();
+    socket.on('connect', function() {
+      console.log('Client connected!');
+    });
+    socket.on('newUser', function(user) {
+      console.log(user);
+      var newUser = user.username;
+      $('#new-connect').prepend(newUser);
+    });
+  })
 });
--- routes/battlefield.js
+++ routes/battlefield.js
@@ -3,6 +3,10 @@ var router = express.Router();
 var passport = require("passport");
 var User = require('../models/user');
 
+// var app = express();
+// var http = require('http').Server(app);
+// var io = require('socket.io')(http);
+
 function authenticatedUser(req, res, next) {
   if (req.isAuthenticated()) return next();
   req.flash('errorMessage', 'Login to access!');
@@ -12,10 +16,13 @@ function authenticatedUser(req, res, next) {
 /* GET battlefield */
 router.get('/', authenticatedUser, function(req, res, next) {
   var user = global.currentUser.local;
+  //set battlefield to true for current user
   User.findOneAndUpdate({ 'local.username': user.username }, { 'local.battlefield': true }, function(err, user) {
     if (err) console.log(err);
+      //socket.emit('newUser', user);
   });
 
+  //find all users in the battlefield & on the same level
   User.find({ 'local.battlefield': true, 'local.level': user.level }, 'local.username local.level', function(err, users) {
     if (err) console.log(err);
       res.render('battlefield', {level: user.level, users: users});
