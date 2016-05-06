95c0b70a877106604a2d68e097b0e4c2b212fd9f
Lauren Zugai <lauren@zugai.com>
2016-05-06 00:20:13 -0500

    maybe no port specified? (heroku)

--- app.js
+++ app.js
@@ -16,9 +16,7 @@ mongoose.connect(process.env.DB_CONN_DRAGON_DUEL);
 
 var app = express();
 var http = require('http').Server(app);
-http.listen(process.env.PORT || 3000, function(){
-  console.log('listening on', http.address().port);
-});
+//http.listen(80);
 var io = require('socket.io')(http);
 
 //when this event fires, execute this callback function
